d6b062052c9cc7f9577e6e3a7c78d537659c48fb
Lewis Daly <lewis@tigerbeetle.com>
2026-07-08 16:51:51 +0200

    add disconnectAll()

--- src/util/consumer.js
+++ src/util/consumer.js
@@ -105,7 +105,6 @@ function consumeWithHealthTracking (consumer, topicName, command) {
  *
  * @description Parses the accountUri into a participant name from the uri string
  *
- * @returns {object} - Returns a Promise
  * @throws {Error} -  if failure occurs
  */
 const createHandler = async (topicName, config, command) => {
@@ -146,6 +145,7 @@ const createHandler = async (topicName, config, command) => {
       connectedTimeStamp
     }
   })
+
 }
 
 /**
@@ -246,6 +246,14 @@ const getMetadataPromise = (consumer, topic) => {
   })
 }
 
+const disconnectAll = async () => {
+  for (const {consumer} of Object.values(listOfConsumers)) {
+    await new Promise((resolve) => {
+      consumer.disconnect(resolve)
+    })
+  }
+}
+
 /**
  * @function allConnected
  *
@@ -306,6 +314,7 @@ const allConnected = async topicName => {
 module.exports = {
   Consumer,
   createHandler,
+  disconnectAll,
   getConsumer,
   getListOfTopics,
   isConsumerAutoCommitEnabled,
